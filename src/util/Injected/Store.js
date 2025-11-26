cdc5ccdb58399f7fa717cb0be3ba998b9bcce665
jix <88824119+jix1351@users.noreply.github.com>
2025-11-26 11:06:05 +0300

    Update Store.js

--- src/util/Injected/Store.js
+++ src/util/Injected/Store.js
@@ -114,7 +114,7 @@ exports.ExposeStore = () => {
     window.Store.Settings = {
         ...window.require('WAWebUserPrefsGeneral'),
         ...window.require('WAWebUserPrefsNotifications'),
-        // setPushname: window.require('WAWebSetPushnameConnAction').setPushname
+        setPushname: window.require('WAWebSetPushnameConnAction').setPushname
     };
     window.Store.NumberInfo = {
         ...window.require('WAPhoneUtils'),
